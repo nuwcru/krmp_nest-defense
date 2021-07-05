ed04e1223b4275a46b477ea7979dd594c8d982b6
nickgulotta77 <56907107+nickgulotta77@users.noreply.github.com>
2021-07-05 15:44:21 -0600

    updated with new explatory views

--- scripts/State_dependence_repeatability_NestDefense.R
+++ scripts/State_dependence_repeatability_NestDefense.R
@@ -35,9 +35,9 @@ boxplot(data$Dives)
 boxplot(data$Score)
 
 require(lattice)
-Z <- cbind(data$Dives, data$MinDisRaw, data$Score)
+Z <- cbind(data$Dives, data$Min, data$Score, data$MinDisRaw*-1)
 
-colnames(Z) <- c("Stoops", "MinDis", "Score")
+colnames(Z) <- c("Stoops", "Min", "Score", "MinDisRaw")
 
 dotplot(as.matrix(Z), groups = FALSE,
         strip = strip.custom(bg = 'white',
@@ -46,6 +46,7 @@ dotplot(as.matrix(Z), groups = FALSE,
                       y = list(relation = "free"),
                       draw = FALSE),
         col = 1, cex  = 0.5, pch = 16,
+        jitter.data=TRUE,
         xlab = "Value of the variable",
         ylab = "Order of the data from text file")
 
@@ -68,7 +69,7 @@ Fig3=ggplot(data,aes(x=Sex, y=MinDisRaw, fill = factor(NestStage)))+
   theme(axis.text.x = element_text(size = 12))+
   theme(axis.text.y = element_text(size = 14))
 
-Fig3+ facet_grid(~Year) + scale_y_continuous(limits = c(0,600))
+Fig3+ facet_grid(~Year) + scale_y_continuous(limits = c(0,100))
 
   #Stoops
 boxplot(Dives~NestStage, data=data)
@@ -130,7 +131,6 @@ histogram( ~ MinDisRaw | NestStage , type = "count",
 
 
   #Stoops
-hist(data$Dives)
 
 histogram( ~ Dives | NestStage , type = "count",
            xlab = "Number of Stoops",
@@ -183,54 +183,114 @@ hist(data$Min1)#backtransformed histogram
 hist(data$MinDis)#original data no transformation
 
 # Year as a factor
-data$Year2<-as.factor(data$Year2)
+data100$Year2<-as.factor(data100$Year2)
 
 
 #Full model----
 
-#data<-subset(data, data$MinDisRaw < 500)
+data100<-subset(data, data$MinDisRaw <= 200)
+
+hist(data100$MinDisRaw)
+data100$MinSq<-sqrt(data100$MinR+0.01)
+hist(data100$MinSq)
+require(glmmTMB)
+
+hist(data100$MinSq)
+
+boxplot(data100$MinR~ data100$ID)
+
+# require(highstat)
+MyVar <- c("Mass.std", "Ivore.std", "Song.std", "DoYr", "Day")
+FD3$E3 <- E3
+MyMultipanel.ggp2(Z = FD3, 
+                  varx = MyVar, 
+                  vary = "E3", 
+                  ylab = "Residuals",
+                  addSmoother = TRUE,
+                  addRegressionLine = FALSE,
+                  addHorizontalLine = TRUE)
+
 
 
 require(lme4)
-m3<-lmer(Min~ -1+Sex + NestStage + Year2 +(1|ID) +
-         (1|ID_Series) +(1|SiteID),
-         data=data)
+library(glmmTMB)
+install.packages("glmmTMB")
+install.packages('TMB', type = 'source')
+m4a<-glmmTMB(MinR~ Sex + NestStage + Year2 +(1|ID) ,
+         data=data100, family=Gamma("log"))
+
+m4<-lmer(MinR~ -1+Sex + NestStage + Year2 +(1|ID) + (1|ID_Series),
+            data=data100)
+m4<-lmer(MinSq~ -1+Sex + NestStage + Year2 +(1|ID) + (1|ID_Series) + (1|SiteID),
+         data=data100)
+
 #summary
-summary(m3)
+summary(m4)
+summary(m4b)
+
+
+table(data$MinDisRaw)
+table(data$Score)
+table(data$Score, data$MinDisRaw)
+table(data$Score, data$MinDisRaw, data$NestStage)
+      
 
 #diagnostic plots----
+plot(m4)
 
-#hist of residuals
-hist(resid(m3)) #residuals normally distributed
-#qq plot 
-lattice::qqmath(m3) #normalilty of errors, residuals appear to be normally distributed
-#residual vs fitted
-plot(m3, type = c("p", "smooth"), col.line = 1) #homogeneity, no patterns&randomly spaced (line almost horizontal)
+
+# Assumption 1-linearity-------
+
+#Min distance values vs residuals
+plot(m4b, MinSq ~ resid(.)) #shows linear relationship
 
 #Min distance values vs fitted
-plot(m3, Min ~ fitted(.), abline = c(0,1)) #shows linear relationship
+plot(m4b, MinSq ~ fitted(.), abline = c(0,1)) #shows linear relationship
+
+
+#Assumption 2-homogeneity of variance---------------
+
+#residual vs fitted
+plot(m4, type = c("p", "smooth"), col.line = 1) #homogeneity, no patterns&randomly spaced (line almost horizontal)
+
+
+
+
+
+
+# Get residuals and fitted values of the lmm
+E1 <- resid(m4b)
+F1 <- fitted(m4b)
+
+
+# Plot residuals versus fitted values.
+par(mfrow = c(1, 1), mar = c(5, 5, 2, 2), cex.lab = 1.5)
+plot(x = F1, 
+     y = E1,
+     xlab = "Fitted values",
+     ylab = "Residuals")
+abline(h = 0, lty = 2, col = 1)
+abline(v=0)
+
+#Assumption 3- Residuals normally distributed----
+
+#hist of residuals
+hist(resid(m4b)) #residuals normally distributed
 
 #random effects histograms
 require(MCMCglmm)
 require(arm)
-hist(ranef(m3)$ID)#normally distributed
-hist(ranef(m3)$ID_Series)#normally distributed
-hist(ranef(m3)$SiteID, breaks= 5) #normally distributed
-
-
-
-#scale location plot
-plot(m3,  
-     sqrt(abs(resid(.)))~fitted(.),
-     type=c("p","smooth"), col.line=1) # mostly horizontal line... homoscedasticity likely satisfied 
-
-
+hist(ranef(m4)$ID)#normally distributed
+hist(ranef(m4)$ID_Series)#normally distributed
+hist(ranef(m4)$SiteID) #normally distributed
+#qq plot 
+lattice::qqmath(m4) #normality of errors
 
 #simulated parameters + repeatability----
 require(dplyr)
 library(arm)
 library(MCMCglmm)
-smod<-sim(m3,1000)
+smod<-sim(m4,1000)
 posterior.mode(as.mcmc(smod@fixef))
 HPDinterval(as.mcmc(smod@fixef))
 ##Between individual variance
@@ -261,12 +321,12 @@ posterior.mode(rvar)
 HPDinterval(rvar)
 
 ### Long-term repeatability
-r<-bvar/(rvar+bvar +bvar2 +bvar3)
+r<-bvar/(rvar+bvar +bvar2 +bvar3 )
 posterior.mode(r)
 HPDinterval(r)  ##repeatability 
 
 ###Short-term
-r1<-(bvar +bvar2 +bvar3)/(rvar+bvar +bvar2+bvar3)
+r1<-(bvar +bvar2 + bvar3 )/(rvar+bvar +bvar2 +bvar3)
 posterior.mode(r1)
 HPDinterval(r1)  ##repeatability
 
@@ -275,7 +335,7 @@ HPDinterval(r1)  ##repeatability
 
 (10^(max(data$Min)  +1.23289)-0.9) # MALE MIN DIS IN METERS
 
-
+(4.93^2)
 
 
 #plot----
@@ -320,6 +380,9 @@ Fig3+ facet_grid(~Year)
 
 
 
+ggplot(aes(x, y), data = d) +
+  geom_point()
+
 
 Fig33=ggplot(data,aes(x=Sex, y=Dives, fill = factor(NestStage)))+
   geom_boxplot()+ 
@@ -333,7 +396,5 @@ Fig33=ggplot(data,aes(x=Sex, y=Dives, fill = factor(NestStage)))+
   theme(axis.text.y = element_text(size = 14))
 
 Fig33+ facet_grid(~Year)
-
-
 
 
